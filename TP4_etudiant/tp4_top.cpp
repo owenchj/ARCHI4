7bba67e4d0e14106ccd47cac59de9061bb3afa82
Jie Chen <owenchj@gmail.com>
2016-10-26 11:30:07 +0200

    Contrôleur IOC et contrôleur d'écran graphique

--- TP4_etudiant/tp4_top.cpp
+++ TP4_etudiant/tp4_top.cpp
@@ -152,7 +152,7 @@ int _main(int argc, char *argv[])
     int     ncycles             = 1000000000;       // simulated cycles
     char    sys_path[256]       = "soft/sys_bin";   // pathname for system code
     char    app_path[256]       = "soft/app_bin";   // pathname for application code
-    char    ioc_filename[256]   = "images.raw";  // pathname for the ioc file
+    char    ioc_filename[256]   = "images.raw";     // pathname for the ioc file
     size_t  fbf_size            = 128;              // number of lines = number of pixels
     bool    debug               = false;            // debug activated
     int     from_cycle          = 0;                // debug start cycle
