f135e8e2619217d4cdee1e378f35607d1a3ff1cd
Jie Chen <owenchj@gmail.com>
2016-10-26 16:50:39 +0200

    reset

--- TP4_etudiant/soft_multi/reset.s
+++ TP4_etudiant/soft_multi/reset.s
@@ -31,21 +31,32 @@ reset:
     la	    $27,    seg_stack_base
     mfc0    $26,    $15,    1
     andi    $10,    $26,    0x7         # $10 <= proc_id
-    sll     $11,    $26,    28          # $11 <= proc_id * 0x01000000
+    sll     $11,    $26,    24          # $11 <= proc_id * 0x01000000
     addu    $27,    $27,    $11         # $27 <= seg_stack_base + proc_id*0x01000000
     li      $26,    0x10000             # $26 <= 64K
     addu    $29,    $26, $27		# $29 <= seg_stack_base + proc_id*0x01000000 + 64K
 
 # initializes interrupt vector
     la      $26,    _interrupt_vector       # interrupt vector address
-    la      $27,    _isr_timer
-    sw      $27,    0($26)              # interrupt_vector[0] <= _isr_timer
-    la      $27,    _isr_tty_get_task0
-    sw      $27,    4($26)              # interrupt_vector[1] <= _isr_tty_get_task0
     la      $27,    _isr_ioc
-    sw      $27,    8($26)              # interrupt_vector[2] <= _isr_ioc
-    la      $27,    _isr_dma
-    sw      $27,    12($26)             # interrupt_vector[3] <= _isr_dma
+    sw      $27,    0($26)              # interrupt_vector[0] <= _isr_ioc
+
+    sw      $27,    16($26)             # interrupt_vector[4] <= _isr_dma
+    sw      $27,    20($26)             # interrupt_vector[5] <= _isr_dma
+    sw      $27,    24($26)             # interrupt_vector[6] <= _isr_dma
+    sw      $27,    28($26)             # interrupt_vector[7] <= _isr_dma
+
+    la      $27,    _isr_timer
+    sw      $27,    32($26)              # interrupt_vector[8] <= _isr_timer
+    sw      $27,    36($26)              # interrupt_vector[9] <= _isr_timer
+    sw      $27,    40($26)              # interrupt_vector[10] <= _isr_timer
+    sw      $27,    44($26)              # interrupt_vector[11] <= _isr_timer
+
+    la      $27,    _isr_tty_get_task0
+    sw      $27,    48($26)              # interrupt_vector[12] <= _isr_tty_get_task0
+    sw      $27,    52($26)              # interrupt_vector[13] <= _isr_tty_get_task0
+    sw      $27,    56($26)              # interrupt_vector[14] <= _isr_tty_get_task0
+    sw      $27,    60($26)              # interrupt_vector[15] <= _isr_tty_get_task0
 
 # initializes ICU
     la      $26,    seg_icu_base
