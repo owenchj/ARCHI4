07f89aac903e0038ddb6b269ee0de84d43a94c8a
Jie Chen <owenchj@gmail.com>
2016-10-26 10:15:17 +0200

    Utilisation du contrôleur TTY

--- TP4_etudiant/soft/main.c
+++ TP4_etudiant/soft/main.c
@@ -5,22 +5,71 @@
  * 0x1 : Timer activated, but no interrupt is generated
  * 0x3 : Timer activarted and periodic interrupts generated
  */
-#define timer_mode 3
+#define not_active_mode  0x0
+#define active_mode      0x1
+#define active_irq_mode  0x3
+
 #define timer_period 500000
 
 __attribute__((constructor)) void main()
 {
     unsigned int ret;
-    ret = timer_set_mode(timer_mode);
-    if(ret)
-    {
-        tty_printf("\ntimer_set_mode error\n");
-    }
 
-    ret = timer_set_period(timer_period);
-    if(ret)
+    /* ret = timer_set_period(timer_period); */
+    /* if(ret) */
+    /* { */
+    /*     tty_printf("\ntimer_set_period error\n"); */
+    /* } */
+
+    /* ret = timer_set_mode(active_irq_mode); */
+    /* if(ret) */
+    /* { */
+    /*     tty_printf("\ntimer_set_mode error\n"); */
+    /* } */
+
+    char byte;
+    while(1)
     {
-        tty_printf("\ntimer_set_period error\n");
+        tty_getc_irq(&byte);
+
+        if(byte == 'a')
+        {
+            ret = timer_set_period(timer_period);
+            if(ret)
+            {
+                tty_printf("\ntimer_set_period error\n");
+            }
+
+            ret = timer_set_mode(active_irq_mode);
+            if(ret)
+            {
+                tty_printf("\ntimer_set_mode error\n");
+            }
+
+            tty_puts("\nTime in active irq mode, with period 500000\n");
+        }
+        else if(byte == 'd')
+        {
+            ret = timer_reset_irq();
+            if(ret)
+            {
+                tty_printf("\ntimer_reset_irq error\n");
+            }
+
+            ret = timer_set_mode(active_mode);
+            if(ret)
+            {
+                tty_printf("\ntimer_set_mode error\n");
+            }
+
+            tty_puts("\nTime not in active mode\n");
+        }
+        else if(byte == 'q')
+        {
+            tty_puts("\nQuit\n");
+            break;
+        }
+
     }
 
     exit();
