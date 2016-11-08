6114bd67347cb86a02f7845f21ac85b347e04671
Jie Chen <owenchj@gmail.com>
2016-11-08 15:04:04 +0100

    Load correct app.bin/sys.bin

--- TP5_etudiant/tp5_cluster_top.cpp
+++ TP5_etudiant/tp5_cluster_top.cpp
@@ -165,8 +165,8 @@ int _main(int argc, char *argv[])
     // command line arguments
     ///////////////////////////////////////////////////////////////
     int     ncycles             = 1000000000;       // simulated cycles
-    char    sys_path[256]       = "soft_hello/sys.bin";   // pathname for system code
-    char    app_path[256]       = "soft_hello/app.bin";   // pathname for application code
+    char    sys_path[256]       = "soft_transpose/sys.bin";   // pathname for system code
+    char    app_path[256]       = "soft_transpose/app.bin";   // pathname for application code
     char    ioc_filename[256]   = "images.raw";  // pathname for the ioc file
     size_t  fbf_size            = 128;              // number of lines = number of pixels
     bool    debug               = false;            // debug activated
